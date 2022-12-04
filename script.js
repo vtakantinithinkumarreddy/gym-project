47e7381158655db132202fba40802136fd82d82f
v.nithinkumarreddy <119048423+vtakantinithinkumarreddy@users.noreply.github.com>
2022-12-05 00:42:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function openForm1() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm1() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+function openForm2() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm2() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openForm3() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm3() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
+function openForm4() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm4() {
+  document.getElementById("myForm").style.display = "none";
+}
+
